dcd488123e15be7a7bacaff247dce1fc24f36ca4
Anoop Christopher Thiparala <30379425+anpct@users.noreply.github.com>
2020-06-26 20:46:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     position: 'bottom center'
   }
 
-  axios.defaults.baseURL = 'http://127.0.0.1:8000';
+  axios.defaults.baseURL = 'https://average-coder-backend.herokuapp.com/';
 
   useEffect(()=>{store.dispatch(loadUser())}, []);
 
